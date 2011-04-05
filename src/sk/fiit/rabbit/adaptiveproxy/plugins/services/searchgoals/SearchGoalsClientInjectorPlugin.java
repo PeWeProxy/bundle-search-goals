b8d130f314118dc95384afe4d21e84f1561a17d5
Ivan Plesko <ivan.plesko@gmail.com>
2011-04-05 22:10:42 +0200

    Inject only when ModifiableSearchResultService is available

--- src/sk/fiit/rabbit/adaptiveproxy/plugins/services/searchgoals/SearchGoalsClientInjectorPlugin.java
+++ src/sk/fiit/rabbit/adaptiveproxy/plugins/services/searchgoals/SearchGoalsClientInjectorPlugin.java
@@ -1,21 +1,17 @@
 package sk.fiit.rabbit.adaptiveproxy.plugins.services.searchgoals;
 
-import java.util.Set;
-
-import sk.fiit.peweproxy.headers.ResponseHeader;
-import sk.fiit.peweproxy.services.ProxyService;
+import sk.fiit.peweproxy.messages.ModifiableHttpResponse;
 import sk.fiit.rabbit.adaptiveproxy.plugins.servicedefinitions.ModifiableSearchResultService;
 import sk.fiit.rabbit.adaptiveproxy.plugins.services.injector.JavaScriptInjectingProcessingPlugin;
 
 public class SearchGoalsClientInjectorPlugin extends JavaScriptInjectingProcessingPlugin {	
 	
-
-	@Override
-	public void desiredResponseServices(
-			Set<Class<? extends ProxyService>> desiredServices,
-			ResponseHeader webRPHeader) {
-		super.desiredResponseServices(desiredServices, webRPHeader);
-		desiredServices.add(ModifiableSearchResultService.class);
+	public ResponseProcessingActions processResponse(ModifiableHttpResponse response) {
+		if (response.getServicesHandle().isServiceAvailable(ModifiableSearchResultService.class)){
+		return super.processResponse(response);
+		} else {
+			return ResponseProcessingActions.PROCEED;
+		}
 	}
 	
 }
