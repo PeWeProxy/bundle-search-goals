6572fe295a46f475f0e9b1b937356bd5d56fa172
Ivan Plesko <ivan.plesko@gmail.com>
2011-04-06 23:47:58 +0200

    Start click tracking development

---
+++ migrations/2011-3-6_23-30_create_search_goals_schema.rb
@@ -0,0 +1,38 @@
+
+class CreateSearchGoalsSchema < ActiveRecord::Migration
+  def self.up
+    create_table :searchgoals_searches do |t|
+      t.integer :id, :limit => 11
+      t.string :uid, :limit => 32
+      t.datetime :timestamp
+      t.string :search_query, :limit => 300
+      t.string :tag, :limit => 100
+    end
+
+    add_index :searchgoals_searches, :uid
+
+    create_table :searchgoals_search_results do |t|
+      t.integer :id, :limit => 11
+      t.string :url, :limit => 4000
+      t.string :heading, :limit => 200
+      t.string :perex, :limit => 500
+      t.integer :id_search, :limit => 11
+    end
+
+    add_index :searchgoals_search_results, :id_search
+
+    create_table :searchgoals_clicked_results do |t|
+      t.integer :id, :limit => 11
+      t.datetime :timestamp
+      t.integer :id_search_result	, :limit => 11
+    end
+
+    add_index :searchgoals_clicked_results, :id_search_result
+  end
+
+  def self.down
+    drop_table :searchgoals_searches
+    drop_table :searchgoals_search_results
+    drop_table :searchgoals_clicked_results
+  end
+end
---
+++ src/sk/fiit/rabbit/adaptiveproxy/plugins/services/searchgoals/ClickTrackingInjectorProcessingPlugin.java
@@ -0,0 +1,143 @@
+package sk.fiit.rabbit.adaptiveproxy.plugins.services.searchgoals;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+import sk.fiit.peweproxy.headers.ResponseHeader;
+import sk.fiit.peweproxy.messages.HttpMessageFactory;
+import sk.fiit.peweproxy.messages.HttpResponse;
+import sk.fiit.peweproxy.messages.ModifiableHttpResponse;
+import sk.fiit.peweproxy.plugins.PluginProperties;
+import sk.fiit.peweproxy.plugins.processing.ResponseProcessingPlugin;
+import sk.fiit.peweproxy.services.ProxyService;
+import sk.fiit.rabbit.adaptiveproxy.plugins.servicedefinitions.DatabaseConnectionProviderService;
+import sk.fiit.rabbit.adaptiveproxy.plugins.servicedefinitions.HtmlDomReaderService;
+import sk.fiit.rabbit.adaptiveproxy.plugins.servicedefinitions.HtmlDomWriterService;
+import sk.fiit.rabbit.adaptiveproxy.plugins.servicedefinitions.ModifiableSearchResultService;
+import sk.fiit.rabbit.adaptiveproxy.plugins.servicedefinitions.SearchResultObject;
+import sk.fiit.rabbit.adaptiveproxy.plugins.services.common.SqlUtils;
+
+public class ClickTrackingInjectorProcessingPlugin implements ResponseProcessingPlugin {
+
+	protected Logger logger = Logger.getLogger(ClickTrackingInjectorProcessingPlugin.class);
+	
+	@Override
+	public void desiredResponseServices(
+			Set<Class<? extends ProxyService>> desiredServices,
+			ResponseHeader webRPHeader) {
+		desiredServices.add(ModifiableSearchResultService.class);
+		desiredServices.add(HtmlDomReaderService.class);
+		desiredServices.add(HtmlDomWriterService.class);
+		
+	}
+
+	@Override
+	public boolean supportsReconfigure(PluginProperties newProps) {
+		return false;
+	}
+
+	@Override
+	public boolean start(PluginProperties props) {
+		return true;
+	}
+
+	@Override
+	public void stop() {
+		
+	}
+
+	@Override
+	public ResponseProcessingActions processResponse(
+			ModifiableHttpResponse response) {
+		if(response.getServicesHandle().isServiceAvailable(ModifiableSearchResultService.class)){
+			ModifiableSearchResultService modifiableSearchResultService = response.getServicesHandle().getService(ModifiableSearchResultService.class);
+			
+			ArrayList<SearchResultObject> searchResultObjectList = modifiableSearchResultService.getSearchedData();
+			int resultCount = searchResultObjectList.size();
+			
+			
+			if (resultCount > 0) {
+				Connection connection = response.getServicesHandle().getService(DatabaseConnectionProviderService.class).getDatabaseConnection();
+
+				String queryString = modifiableSearchResultService.getQueryString().trim();
+				
+				int searchResultID = insertSearchToDB(connection, queryString);
+				
+				for (SearchResultObject searchResultObject : searchResultObjectList){
+					modifiableSearchResultService.deleteResult(1);
+					modifiableSearchResultService.putResult(new SearchResultObject(searchResultObject, "alert('TOTO: log what was clicked')"), resultCount);
+					
+					insertSearchResultToDB(connection, searchResultObject, searchResultID);
+					
+				}
+				
+				SqlUtils.close(connection);
+			}
+		}
+		
+		return ResponseProcessingActions.PROCEED;
+	}
+
+	private void insertSearchResultToDB(Connection connection, SearchResultObject searchResultObject, int searchResultID){
+		try {
+			PreparedStatement stmt = connection.prepareStatement("INSERT INTO `searchgoals_search_results` (`url`, `heading`, `perex`, `id_search`) VALUES (?, ?, ?, ?);");
+			
+			stmt.setString(1, searchResultObject.getUrl());
+			stmt.setString(2, searchResultObject.getHeader());
+			stmt.setString(3, searchResultObject.getPerex());
+			stmt.setInt(4, searchResultID);
+			
+			stmt.execute();
+		} catch (SQLException e) {
+			logger.error("Error inserting search result for search id "+searchResultID+" to database");
+		}
+	}
+	
+	private int insertSearchToDB(Connection connection, String queryString){
+		int searchResultID = -1;
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement("INSERT INTO `searchgoals_searches` (`timestamp`, `search_query`) VALUES (?, ?);", Statement.RETURN_GENERATED_KEYS);
+			
+			stmt.setString(1, getFormatedTimestamp());
+			stmt.setString(2, queryString);
+			stmt.execute();
+			
+			ResultSet set = stmt.getGeneratedKeys();
+			
+			if (set.next()){
+				searchResultID = set.getInt(1);
+			}
+			
+		} catch (SQLException e) {
+			logger.error("Error inserting search result for search id to database");
+		}
+		
+		return searchResultID;
+	}
+	
+	private String getFormatedTimestamp(){
+		java.util.Date today = new java.util.Date();
+		String timestamp = new Timestamp(today.getTime()).toString();
+		return timestamp.substring(0, timestamp.indexOf("."));
+	}
+	
+	@Override
+	public HttpResponse getNewResponse(ModifiableHttpResponse response,
+			HttpMessageFactory messageFactory) {
+		return null;
+	}
+
+	@Override
+	public void processTransferedResponse(HttpResponse response) {
+	}
+
+}
